f10eec43f8e3bbf52ba5567cd8c2eafd4e767c4a
Eugen Karaman <karamanjekakol@gmail.com>
2024-06-19 13:55:14 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _28._05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateSum_Click(object sender, RoutedEventArgs e)
+        {
+            string input = inputTextBox.Text;
+            string[] inputs = input.Split(',');
+
+            double sum = 0;
+            foreach (string s in inputs)
+            {
+                if (double.TryParse(s.Trim(), out double value))
+                {
+                    sum += value;
+                }
+                else
+                {
+                    MessageBox.Show($"Incorrect value: {s}", "ERROR!!!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+
+            resultTextBox.Text = sum.ToString("F2");
+        }
+    }
+}
